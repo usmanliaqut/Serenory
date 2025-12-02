4812db638dde6d1ecfa22782956d6bbfc912573f
usman-liaqut <usman.liaqat@trailfive.com>
2025-12-02 18:12:28 +0500

    button added

--- components/Common/ HeroSection.tsx
+++ components/Common/ HeroSection.tsx
@@ -125,7 +125,7 @@ const HeroSection: React.FC<CTASectionProps> = ({ onBookSession }) => {
             >
               <div className="absolute inset-0 bg-gradient-to-r from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
               <Calendar className="w-6 h-6 mr-3" />
-              Book Session
+              Book your session
             </Button>
 
             <button
